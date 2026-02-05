c235d7d815f4e098b465b7870f765a7a11cf945a
quincy-j <quin.cjefferson@gmail.com>
2026-02-05 12:49:17 +0100

    refactor code into functions and apply PEP8

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,39 +1,57 @@
 import json
 
+
 def load_data(filename):
     with open(filename, "r") as file:
         return json.load(file)
 
-animals = load_data("animals_data.json")
 
-output = ""
-
-for animal in animals:
+def serialize_animal(animal):
     name = animal.get("name")
     diet = animal.get("characteristics", {}).get("diet")
     type_ = animal.get("characteristics", {}).get("type")
     locations = animal.get("locations", [])
     location = locations[0] if locations else None
 
-    output += '<li class="cards__item">\n'
-    output += f'<div class="card__title">{name}</div>\n'
-    output += '<p class="card__text">'
+    html = '<li class="cards__item">\n'
+    html += f'<div class="card__title">{name}</div>\n'
+    html += '<p class="card__text">'
 
     if diet:
-        output += f'<strong>Diet:</strong> {diet}<br/>\n'
+        html += f'<strong>Diet:</strong> {diet}<br/>\n'
     if location:
-        output += f'<strong>Location:</strong> {location}<br/>\n'
+        html += f'<strong>Location:</strong> {location}<br/>\n'
     if type_:
-        output += f'<strong>Type:</strong> {type_}<br/>\n'
+        html += f'<strong>Type:</strong> {type_}<br/>\n'
 
-    output += '</p></li>\n'
+    html += '</p></li>\n'
+    return html
 
-with open("animals_template.html", "r") as file:
-    template = file.read()
 
-html_output = template.replace("__REPLACE_ANIMALS_INFO__", output)
+def build_html_page(data, template_path, output_path):
+    items_html = ""
 
-with open("animals.html", "w") as file:
-    file.write(html_output)
+    for animal in data:
+        items_html += serialize_animal(animal)
 
-print("animals.html wurde aktualisiert.")
+    with open(template_path, "r") as file:
+        template = file.read()
+
+    final_html = template.replace("__REPLACE_ANIMALS_INFO__", items_html)
+
+    with open(output_path, "w") as file:
+        file.write(final_html)
+
+
+def main():
+    animals = load_data("animals_data.json")
+    build_html_page(
+        data=animals,
+        template_path="animals_template.html",
+        output_path="animals.html"
+    )
+    print("animals.html wurde aktualisiert.")
+
+
+if __name__ == "__main__":
+    main()
