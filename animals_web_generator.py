57697d4fbc660594665a102b1cb4fe5fa3bd2292
quincy-j <quin.cjefferson@gmail.com>
2026-02-05 12:31:49 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,40 +4,35 @@ def load_data(filename):
     with open(filename, "r") as file:
         return json.load(file)
 
-# 1. JSON laden
 animals = load_data("animals_data.json")
 
-# 2. HTML-String erzeugen
 output = ""
 
 for animal in animals:
     name = animal.get("name")
-    diet = animal.get("diet")
+    diet = animal.get("characteristics", {}).get("diet")
+    type_ = animal.get("characteristics", {}).get("type")
     locations = animal.get("locations", [])
     location = locations[0] if locations else None
-    type_ = animal.get("type")
 
     output += '<li class="cards__item">\n'
+    output += f'<div class="card__title">{name}</div>\n'
+    output += '<p class="card__text">'
 
-    if name:
-        output += f"Name: {name}<br/>\n"
     if diet:
-        output += f"Diet: {diet}<br/>\n"
+        output += f'<strong>Diet:</strong> {diet}<br/>\n'
     if location:
-        output += f"Location: {location}<br/>\n"
+        output += f'<strong>Location:</strong> {location}<br/>\n'
     if type_:
-        output += f"Type: {type_}<br/>\n"
+        output += f'<strong>Type:</strong> {type_}<br/>\n'
 
-    output += '</li>\n\n'
+    output += '</p></li>\n'
 
-# 3. Template laden
 with open("animals_template.html", "r") as file:
     template = file.read()
 
-# 4. Platzhalter ersetzen
 html_output = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
-# 5. Neue Datei schreiben
 with open("animals.html", "w") as file:
     file.write(html_output)
 
